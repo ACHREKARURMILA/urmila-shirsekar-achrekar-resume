895322bb8fd6a93d1e62a6107a4f19972030b8bb
ACHREKARURMILA <u.achrekar27@gmail.com>
2020-12-13 17:35:29 +0530

    Updates

---
+++ precache-manifest.602e864bec1ce6870895a529f17393a2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35a557cfdfd1c826dfa8106a4711b428",
+    "url": "/urmila-shirsekar-achrekar-resume/index.html"
+  },
+  {
+    "revision": "025fbb2dfe7911b81327",
+    "url": "/urmila-shirsekar-achrekar-resume/static/css/main.6038fa5a.chunk.css"
+  },
+  {
+    "revision": "8cd2df8b2345486e2ff7",
+    "url": "/urmila-shirsekar-achrekar-resume/static/js/2.7a224ce2.chunk.js"
+  },
+  {
+    "revision": "3143274d7d26932c98894a52e75c487a",
+    "url": "/urmila-shirsekar-achrekar-resume/static/js/2.7a224ce2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "025fbb2dfe7911b81327",
+    "url": "/urmila-shirsekar-achrekar-resume/static/js/main.b71d2736.chunk.js"
+  },
+  {
+    "revision": "f022410dbc851fb79f2c",
+    "url": "/urmila-shirsekar-achrekar-resume/static/js/runtime-main.2bd5b882.js"
+  },
+  {
+    "revision": "4e1cf6fff3b682abbab61ee91b1b81bf",
+    "url": "/urmila-shirsekar-achrekar-resume/static/media/urmila.4e1cf6ff.png"
+  }
+]);
